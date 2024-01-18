08264bbb8587b0bdabe1e154a50c994da1ad4b5e
yashmodi998 <yashmodi998@gmail.com>
2024-01-17 19:26:05 -0500

    changes in index.php

--- Labs/week2/index.php
+++ Labs/week2/index.php
@@ -9,16 +9,16 @@
   
    <?php echo ' <h1>PHP and Creating Output</h1>';?>
   
-    <p>The PHP echo command can be used to create output.</p>
+   <?php echo ' <p>The PHP echo command can be used to create output.</p>';?>
 
     <p>When creating output using echo and PHP, quotes can often cause problems. There are several solutions to using quotes within an echo statement:</p>
-    
+    <?php echo'
     <ul>
         <li>Use HTML special characters</li>
         <li>Alternate between single and double quotes</li>
         <li>Use a backslash to escape quotes</li>
-    </ul>
-    
+    </ul>';
+    ?>
     <h2>More HTML to Convert</h2>
 
     <p>PHP says "Hello World!"</p>
